7d49f41f547adacc2f4621e7042344dc01ad979a
Sean Grant <seangrant118@gmail.com>
2020-03-09 20:21:55 -0400

    mongoose models

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const port = process.env.PORT || 3090;
 const app = express();
+const router = require("./router");
+
+//App setup
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
 
 //Server Setup
 app.listen(port);
