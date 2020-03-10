a6cac2166f5de84c4cc2a136cc4518845216628e
Sean Grant <seangrant118@gmail.com>
2020-03-10 12:32:23 -0400

    authenticated request

--- server/router.js
+++ server/router.js
@@ -1,5 +1,13 @@
 const Authentication = require("./controllers/authentication");
+const passportService = require("./services/passport");
+const passport = require("passport");
+
+// Authenticate using jwt, not using a cookie based session
+const requireAuth = passport.authenticate("jwt", { session: false });
 
 module.exports = function(app) {
+  app.get("/", requireAuth, function(req, res) {
+    res.send({ hi: "there" });
+  });
   app.post("/signup", Authentication.signup);
 };
