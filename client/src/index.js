36544899788ff22f6d09eb510129dec247677619
Sean Grant <seangrant118@gmail.com>
2020-03-11 11:40:02 -0400

    persisting login state

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,15 @@ import Signup from "./components/auth/Signup";
 import reducers from "./reducers";
 import Feature from "./components/Feature";
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(
+  reducers,
+  {
+    auth: {
+      authenticated: localStorage.getItem("token")
+    }
+  },
+  applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
