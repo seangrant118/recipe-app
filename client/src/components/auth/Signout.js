b470d69a68fd731729570682d3dbefd878fa9744
Sean Grant <seangrant118@gmail.com>
2020-03-11 11:46:36 -0400

    automatic signout

--- client/src/components/auth/Signout.js
+++ client/src/components/auth/Signout.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 class Signout extends React.Component {
+  componentDidMount() {
+    this.props.signout();
+  }
   render() {
     return <div>sorry to see you go!</div>;
   }
 }
 
-export default Signout;
+export default connect(null, actions)(Signout);
