a06a572fe42abf2704f98d9f761228afc0c47256
Sean Grant <seangrant118@gmail.com>
2020-03-16 16:24:16 -0400

    ingredients

--- client/src/components/CreateRecipe.js
+++ client/src/components/CreateRecipe.js
@@ -13,7 +13,14 @@ class CreateRecipe extends React.Component {
       time: "",
       unit: "Mins"
     },
-    servings: 0
+    servings: 0,
+    ingredients: [
+      {
+        quantity: "",
+        unit: "",
+        ingredient: ""
+      }
+    ]
   };
   titleChange = e => {
     let title = e.target.value;
@@ -57,7 +64,29 @@ class CreateRecipe extends React.Component {
       servings
     });
   };
+  addIngredient = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      ingredients: [
+        ...prevState.ingredients,
+        { quantity: "", unit: "", ingredient: "" }
+      ]
+    }));
+  };
+  ingredientChange = e => {
+    if (["quantity", "unit", "ingredient"].includes(e.target.className)) {
+      let ingredients = [...this.state.ingredients];
+      ingredients[e.target.dataset.id][
+        e.target.className
+      ] = e.target.value.toUpperCase();
+      this.setState({ ingredients }, () => console.log(this.state.ingredients));
+    } else {
+      this.setState({ [e.target.name]: e.target.value.toUpperCase() });
+      console.log(e.target);
+    }
+  };
   render() {
+    let { ingredients } = this.state;
     return (
       <form>
         <fieldset>
@@ -96,8 +125,6 @@ class CreateRecipe extends React.Component {
             <option value="Mins">Mins</option>
             <option value="Hrs">Hrs</option>
           </select>
-        </fieldset>
-        <fieldset>
           <label>Prep Time</label>
           <input
             name="prep time"
@@ -123,6 +150,41 @@ class CreateRecipe extends React.Component {
             value={this.state.servings}
             onChange={this.servingChange}
           />
+        </fieldset>
+        <fieldset onChange={this.ingredientChange}>
+          <button onClick={this.addIngredient}>Add New Ingredient</button>
+          {ingredients.map((ingredient, i) => {
+            let ingredientID = `ingredient-${i}`;
+            let quantityID = `quantity-${i}`;
+            let unitID = `unit-${i}`;
+
+            return (
+              <div key={i}>
+                <label>{`Ingredient ${i + 1}`}</label>
+                <label htmlFor={quantityID}>Quantity</label>
+                <input
+                  type="number"
+                  name={quantityID}
+                  data-id={i}
+                  id={quantityID}
+                  className="quantity"
+                />
+                <label htmlFor={unitID}>Unit</label>
+                <select name={unitID} data-id={i} id={unitID} className="unit">
+                  <option value="cups">cups</option>
+                  <option value="tbs">tbs</option>
+                </select>
+                <label htmlFor={ingredientID}>Ingredient</label>
+                <input
+                  type="text"
+                  name={ingredientID}
+                  data-id={i}
+                  id={ingredientID}
+                  className="ingredient"
+                />
+              </div>
+            );
+          })}
         </fieldset>
       </form>
     );
