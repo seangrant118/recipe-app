284bbdf0bcd2c5a8f4073e754cf51dbb7918607e
Sean Grant <seangrant118@gmail.com>
2020-03-17 18:52:34 -0400

    recipe view page

--- client/src/components/RecipeView.js
+++ client/src/components/RecipeView.js
@@ -2,8 +2,10 @@ import React from "react";
 import axios from "axios";
 import { withRouter } from "react-router";
 
-const RecipeView = props => {
-  return <div>viewing recipe with the id of {props.match.params.id}</div>;
-};
+class RecipeView extends React.Component {
+  render() {
+    return <div>viewing recipe with id of {this.props.match.params.id}</div>;
+  }
+}
 
 export default withRouter(RecipeView);
