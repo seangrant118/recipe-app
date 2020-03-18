dfdce217b7c1702d7e061dcb8098557abfb7b553
Sean Grant <seangrant118@gmail.com>
2020-03-18 11:23:20 -0400

    recipeview skelly

--- client/src/components/RecipeView.js
+++ client/src/components/RecipeView.js
@@ -18,7 +18,33 @@ class RecipeView extends React.Component {
   render() {
     const recipe = this.props.recipe;
     if (this.state.loaded) {
-      return <div>Title: {recipe.title}</div>;
+      return (
+        <div>
+          <div>Title: {recipe.title}</div>
+          <div>Description: {recipe.description}</div>
+          <div>
+            Cook Time: {recipe.cookTime} {recipe.cookTimeUnit}
+          </div>
+          <div>
+            Prep Time: {recipe.prepTime} {recipe.prepTimeUnit}
+          </div>
+          <div>Servings: {recipe.servings}</div>
+          <div>Ingredients:</div>
+          {recipe.ingredients.map((ingredient, i) => {
+            return (
+              <li key={i}>
+                {ingredient.quantity} {ingredient.unit} {ingredient.ingredient}
+              </li>
+            );
+          })}
+          <div>Instructions: </div>
+          <ol>
+            {recipe.steps.map((step, i) => {
+              return <li key={i}>{step.step}</li>;
+            })}
+          </ol>
+        </div>
+      );
     } else {
       return <div>Loading...</div>;
     }
