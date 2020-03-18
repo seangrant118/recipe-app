394f3070ea8c6207d240a69f4a07b9095247ae3d
Sean Grant <seangrant118@gmail.com>
2020-03-18 10:05:37 -0400

    get recipe reducer

--- client/src/reducers/recipe.js
+++ client/src/reducers/recipe.js
@@ -1,8 +1,14 @@
-import { CREATE_RECIPE, CREATE_ERROR } from "../actions/recipe/types";
+import {
+  CREATE_RECIPE,
+  CREATE_ERROR,
+  GET_RECIPE
+} from "../actions/recipe/types";
 
 export default function(state = {}, action) {
   switch (action.type) {
     case CREATE_RECIPE:
+      return { ...state, recipe: action.payload };
+    case GET_RECIPE:
       return { ...state, recipe: action.payload };
     case CREATE_ERROR:
       return { ...state, errorMessage: action.payload };
