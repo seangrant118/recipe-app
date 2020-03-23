2a2c11870fef8715c70581cbaa9be33ef9722da6
Sean Grant <seangrant118@gmail.com>
2020-03-23 11:49:30 -0400

    layout title

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,7 +6,7 @@ const Landing = () => {
     <div className="layout">
       <div className="layout-container">
         <h1 className="layout-title">Ma's Cookbook</h1>
-        <p>Recipes so good I had to share</p>
+        <p className="layout-subtitle">Recipes so good I had to share</p>
         <button className="btn-browse">
           <a className="a-browse" href="/home">
             Begin Browsing Recipes
