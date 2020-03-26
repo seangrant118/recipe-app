77c6ebaa873f8de0007ce4dfeb07cd6bd6c987d7
Sean Grant <seangrant118@gmail.com>
2020-03-26 09:25:02 -0400

    landing desktop

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,13 +5,14 @@ const Landing = () => {
   return (
     <div className="layout">
       <div className="layout-container">
-        <h1 className="layout-title">Ma's Cookbook</h1>
         <p className="layout-subtitle">Recipes so good I had to share</p>
-        <button className="btn-browse">
-          <a className="a-browse" href="/home">
-            Begin Browsing Recipes
-          </a>
-        </button>
+        <div class="btn-container">
+          <button className="btn-browse">
+            <a className="a-browse" href="/home">
+              Begin Browsing Recipes
+            </a>
+          </button>
+        </div>
       </div>
     </div>
   );
