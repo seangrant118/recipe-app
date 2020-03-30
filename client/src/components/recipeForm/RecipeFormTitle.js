9e52074002068837187bcdc53c7bd5b1634246cd
Sean Grant <seangrant118@gmail.com>
2020-03-30 14:59:23 -0400

    box shaddow label container

--- client/src/components/recipeForm/RecipeFormTitle.js
+++ client/src/components/recipeForm/RecipeFormTitle.js
@@ -3,7 +3,9 @@ import React from "react";
 const RecipeFormTitle = props => {
   return (
     <fieldset className="recipe-form-field">
-      <label className="recipe-form-label">Title</label>
+      <div className="label-container">
+        <label className="recipe-form-label">Title</label>
+      </div>
       <input
         className="recipe-form-input"
         name="title"
