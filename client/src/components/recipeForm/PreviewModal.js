f6c77a447449e423f68e63b89c1f642b8bb00f56
Sean Grant <seangrant118@gmail.com>
2020-04-01 14:54:38 -0400

    modal container

--- client/src/components/recipeForm/PreviewModal.js
+++ client/src/components/recipeForm/PreviewModal.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Modal from "react-modal";
+import { MdFullscreenExit as Exit } from "react-icons/md";
+import "../styles/recipeForm/PreviewModal.css";
+
 const style = {
   width: "50px",
   height: "50px"
 };
+
+Modal.defaultStyles = {};
 const PreviewModal = props => (
   <Modal
     isOpen={props.isOpen}
@@ -14,38 +19,43 @@ const PreviewModal = props => (
     ariaHideApp={false}
   >
     <div>
-      <h1>{props.state.title}</h1>
-      <p>{props.state.description}</p>
-      <img style={style} src={props.state.image} alt="recipe" />
-      <p>
-        Cook Time: {props.state.cookTime} {props.state.cookTimeUnit}
-      </p>
-      <p>
-        Prep Time: {props.state.prepTime} {props.state.prepTimeUnit}
-      </p>
-      <p>Servings: {props.state.servings}</p>
-      <h5>Ingredients</h5>
-      <ul>
-        {props.state.ingredients.map((ingredient, i) => {
-          return (
-            <li key={i}>
-              {ingredient.quantity} {ingredient.unit} {ingredient.ingredient}
-            </li>
-          );
-        })}
-      </ul>
-      <h5>Instructions</h5>
-      <ol>
-        {props.state.steps.map((step, i) => {
-          return (
-            <li key={i}>
-              Step {i + 1}: {step.step}
-            </li>
-          );
-        })}
-      </ol>
-      <button onClick={props.handleCloseModal}>Close Modal</button>
-      <button onClick={props.submitRecipe}>Submit Recipe!</button>
+      <div className="exit-btn" onClick={props.handleCloseModal}>
+        x
+      </div>
+      <div className="modal-container">
+        <h1>{props.state.title}</h1>
+        <p>{props.state.description}</p>
+        <img style={style} src={props.state.image} alt="recipe" />
+        <p>
+          Cook Time: {props.state.cookTime} {props.state.cookTimeUnit}
+        </p>
+        <p>
+          Prep Time: {props.state.prepTime} {props.state.prepTimeUnit}
+        </p>
+        <p>Servings: {props.state.servings}</p>
+        <h5>Ingredients</h5>
+        <ul>
+          {props.state.ingredients.map((ingredient, i) => {
+            return (
+              <li key={i}>
+                {ingredient.quantity} {ingredient.unit} {ingredient.ingredient}
+              </li>
+            );
+          })}
+        </ul>
+        <h5>Instructions</h5>
+        <ol>
+          {props.state.steps.map((step, i) => {
+            return (
+              <li key={i}>
+                Step {i + 1}: {step.step}
+              </li>
+            );
+          })}
+        </ol>
+        <button onClick={props.handleCloseModal}>Close Modal</button>
+        <button onClick={props.submitRecipe}>Submit Recipe!</button>
+      </div>
     </div>
   </Modal>
 );
