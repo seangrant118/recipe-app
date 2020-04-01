69030c81596b1bb223b5336a067b2baba2c217c8
Sean Grant <seangrant118@gmail.com>
2020-04-01 09:22:24 -0400

    file input ref

--- client/src/components/recipeForm/RecipeFormImage.js
+++ client/src/components/recipeForm/RecipeFormImage.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const RecipeFormImage = props => {
+  const fileInput = useRef(null);
+
+  function handleClick() {
+    fileInput.current.click();
+  }
+
   return (
     <fieldset className="recipe-form-field">
-      <label className="recipe-form-label">Add an Image</label>
-      <input onChange={props.handleImageChange} type="file" />
+      <div className="label-container">
+        <label className="recipe-form-label">Add an Image</label>
+      </div>
+      <input
+        style={{ display: "none" }}
+        onChange={props.handleImageChange}
+        type="file"
+        ref={fileInput}
+      />
+      <button onClick={handleClick}>Choose File</button>
       <img src={props.image} alt="recipe" />
     </fieldset>
   );
