c8af3d9bee6cdb0025b26d805b2f16675716869b
Sean Grant <seangrant118@gmail.com>
2020-04-08 14:44:33 -0400

    burger menu functionality

--- client/src/components/BurgerButton.js
+++ client/src/components/BurgerButton.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class BurgerButton extends React.Component {
   state = {
@@ -10,10 +11,44 @@ class BurgerButton extends React.Component {
   }
   render() {
     return (
-      <div className="burger-container" onClick={() => this.handleClick()}>
-        <div className="line line-top" />
-        <div className="line line-middle" />
-        <div className="line line-bottom" />
+      <div>
+        <div className="burger-container" onClick={() => this.handleClick()}>
+          <div className="line line-top" />
+          <div className="line line-middle" />
+          <div className="line line-bottom" />
+        </div>
+        {this.state.open && (
+          <div className="menu-container">
+            <div className="exit-container" onClick={() => this.handleClick()}>
+              <div className="exit exit-top" />
+              <div className="exit exit-middle" />
+              <div className="exit exit-bottom" />
+            </div>
+            <div className="menu-list">
+              <Link
+                className="burger-item"
+                to="/home"
+                onClick={() => this.handleClick()}
+              >
+                Home
+              </Link>
+              <Link
+                className="burger-item"
+                to="/signout"
+                onClick={() => this.handleClick()}
+              >
+                Sign Out
+              </Link>
+              <Link
+                className="burger-item"
+                to="/createrecipe"
+                onClick={() => this.handleClick()}
+              >
+                Create Recipe
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
