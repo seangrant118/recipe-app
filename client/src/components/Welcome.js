2aa984dca5a2df3e412bc13ce5a648451c04da0b
Sean Grant <seangrant118@gmail.com>
2021-03-25 15:19:57 -0400

    client side data response

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,15 +1,34 @@
 import React from "react";
+import axios from "axios";
 import "./styles/welcome.css";
 
-export default () => {
-  return (
-    <div className="welcome-container">
-      <h1 className="welcome-header">
-        Welcome! Sign in or Sign up to create a recipe
-      </h1>
-      <h3 className="welcome-search">
-        Search for great recipes by name or ingredient
-      </h3>
-    </div>
-  );
-};
+class Welcome extends React.Component {
+  state = {
+    data: null,
+  };
+  componentDidMount() {
+    const getData = async () => {
+      const response = await axios.get("http://localhost:3090/recipe/random");
+      this.setState({
+        data: response.data,
+      });
+    };
+
+    getData();
+  }
+
+  render() {
+    return (
+      <div className="welcome-container">
+        <h1 className="welcome-header">
+          Welcome! Sign in or Sign up to create a recipe
+        </h1>
+        <h3 className="welcome-search">
+          Search for great recipes by name or ingredient
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
