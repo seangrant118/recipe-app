04c0fa7f5e4c1dabe5b9e663f0afb127a424b36c
Sean Grant <seangrant118@gmail.com>
2021-03-26 11:23:03 -0400

    redirect on signout

--- client/src/components/auth/Signout.js
+++ client/src/components/auth/Signout.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as actions from "../../actions/auth";
+import "../styles/auth/signout.css";
 
 class Signout extends React.Component {
   componentDidMount() {
     this.props.signout();
   }
+
   render() {
-    return <div>sorry to see you go!</div>;
+    return (
+      <div>
+        <div className="sorry">sorry to see you go!</div>
+        <button onClick={this.props.history.push("/home")}>Home</button>
+      </div>
+    );
   }
 }
 
