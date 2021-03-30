4b539d14ae49d0d1b67d07acff0420e8da3cfc26
Sean Grant <seangrant118@gmail.com>
2021-03-30 14:38:51 -0400

    env variables

--- client/src/fb/index.js
+++ client/src/fb/index.js
@@ -3,14 +3,14 @@ import secret from "./secret";
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: secret.apiKey,
-  authDomain: secret.authDomain,
-  databaseURL: secret.databaseURL,
-  projectId: secret.projectId,
-  storageBucket: secret.storageBucket,
-  messagingSenderId: secret.messagingSenderId,
-  appId: secret.appId,
-  measurementId: secret.measurementId,
+  apiKey: process.env.apiKey || secret.apiKey,
+  authDomain: process.env.authDomain || secret.authDomain,
+  databaseURL: process.env.databaseURL || secret.databaseURL,
+  projectId: process.env.projectId || secret.projectId,
+  storageBucket: process.env.storageBucket || secret.storageBucket,
+  messagingSenderId: process.env.messagingSenderId || secret.messagingSenderId,
+  appId: process.env.appId || secret.appId,
+  measurementId: process.env.measurementId || secret.measurementId,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
